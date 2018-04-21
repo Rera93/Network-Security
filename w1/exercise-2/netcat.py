b9fbf83c27e70d4f63001e84b4960bb469efd837
Brigel Pineti <b.pineti@student.ru.nl>
2018-04-22 00:55:03 +0200

    handling data on sending side, (sending(infite loop) and receving)

--- w1/exercise-2/netcat.py
+++ w1/exercise-2/netcat.py
@@ -3,6 +3,15 @@
 import socket
 import sys
 
+def handle_data(udp_socket):
+    while(1):
+        message_to_send = input("Enter message to send: ")
+        encoded_message = message_to_send.encode("utf-8")
+        udp_socket.sendto(encoded_message, (host, port))
+        non_decoded_server_message, server_address = udp_socket.recvfrom(size)
+        decoded_server_message = non_decoded_server_message.decode("utf-8")
+        print("Server replied: " + decoded_server_message + "\n")
+
 def main():
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -11,6 +20,7 @@ def main():
         print("\nFailed to create UDP socket. \n")
         sys.exit()
 
+    handle_data(s)
     s.close()
 
 if __name__ == "__main__":  
