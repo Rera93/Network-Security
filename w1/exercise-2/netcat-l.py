7de4136d38b2c0b4cc59908a44ff440c3080ee6b
Brigel Pineti <b.pineti@student.ru.nl>
2018-04-22 00:32:08 +0200

    implement try, except for binding of UDP socket

--- w1/exercise-2/netcat-l.py
+++ w1/exercise-2/netcat-l.py
@@ -9,6 +9,14 @@ def main():
         print("\nUDP socket is created...\n")
     except(socket.error):
         print("\nFailed to create UDP socket. \n")
+        sys.exit()
+
+    try:
+        s.bind((host, port))
+        print(host + " is bound to UDP socket at port " + str(port) +".\n")
+    except(socket.error):
+        print("Binding " + host + " at port " + str(port) + " has failed.\n")
+        sys.exit()
 
 if __name__ == "__main__":
     host = "localhost"
