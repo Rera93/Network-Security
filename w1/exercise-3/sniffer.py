07fd9c54ab0813c09dff5922906a152911b02573
Brigel Pineti <b.pineti@student.ru.nl>
2018-04-23 23:27:15 +0200

    unpacking header with struct.unpack()

--- w1/exercise-3/sniffer.py
+++ w1/exercise-3/sniffer.py
@@ -1,11 +1,14 @@
 #!usr/bin/env python3
 
 import socket
+import struct
 
 def parse_udp(packet):
     header_start = 0
     header_length = 8
     header = packet[header_start:header_start+header_length]
+
+    (source_port, dest_port, data_length, checksum) = struct.unpack("!HHHH", header)
 
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP)
