887857db2cb823c001c70e435ed0de457298fec7
Brigel Pineti <b.pineti@student.ru.nl>
2018-04-23 23:21:50 +0200

    create raw socket and iterate though UDP/IP packets

--- w1/exercise-3/sniffer.py
+++ w1/exercise-3/sniffer.py
@@ -3,8 +3,11 @@
 import socket
 
 def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP)
+    while(1):
+        print(s.recvfrom(size))
 
-
-
-if __name__ == "__main__"
+if __name__ == "__main__":
+    size = 66565 
     main()
+    
