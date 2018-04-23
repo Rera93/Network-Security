376a3c657ade40cb55f6fa2a12748f913121b882
Brigel Pineti <b.pineti@student.ru.nl>
2018-04-23 23:24:34 +0200

    slicing header from UDP packet

--- w1/exercise-3/sniffer.py
+++ w1/exercise-3/sniffer.py
@@ -2,6 +2,11 @@
 
 import socket
 
+def parse_udp(packet):
+    header_start = 0
+    header_length = 8
+    header = packet[header_start:header_start+header_length]
+
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP)
     while(1):
