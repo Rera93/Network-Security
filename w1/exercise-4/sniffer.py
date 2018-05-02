7bba67f6f564c82920fcfc1099e014256a7cfc80
Brigel Pineti <b.pineti@student.ru.nl>
2018-05-02 19:34:35 +0200

    changed socket definition to include other packets || commented part of code in main

--- w1/exercise-4/sniffer.py
+++ w1/exercise-4/sniffer.py
@@ -28,28 +28,25 @@ def parse_ip(packet):
     return header_length_in_bytes, header, total_length, protocol, source_address, dest_address, data
 
 def main():
-    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP)
+    s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
 
     while True:
         # client_packet = bytes object representing the data received
         # address = address of the socket sending the data
         client_packet, address = s.recvfrom(size)
 
-        header_length, ip_header_content, total_length, protocol, source_address, dest_address, ip_data = parse_ip(client_packet)
+#        header_length, ip_header_content, total_length, protocol, source_address, dest_address, ip_data = parse_ip(client_packet)
+#
+#        print("### IP Header ###\n"
+#              "Total Length: {}\nProtocol: {}\n"
+#              "Source IP Address: {}\n"
+#              "Destination IP Address: {}\n".format(
+#                  total_length, protocol, socket.inet_ntoa(source_address), socket.inet_ntoa(dest_address)))
 
-        print("### IP Header ###\n"
-              "Total Length: {}\nProtocol: {}\n"
-              "Source IP Address: {}\n"
-              "Destination IP Address: {}\n".format(
-                  total_length, protocol, socket.inet_ntoa(source_address), socket.inet_ntoa(dest_address)))
-
-        source_port, dest_port, data_length, checksum, udp_data = parse_udp(ip_header_content)        
-        print("Source Port: {}\nDestination Port: {}\n"
-              "Data Length: {}\nChecksum: {}\nUDP Data: {}\n".format(
-                  source_port, dest_port, data_length, checksum, udp_data))
-
-
-
+#        source_port, dest_port, data_length, checksum, udp_data = parse_udp(ip_header_content)        
+#        print("Source Port: {}\nDestination Port: {}\n"
+#              "Data Length: {}\nChecksum: {}\nUDP Data: {}\n".format(
+#                  source_port, dest_port, data_length, checksum, udp_data))
 
 if __name__ == "__main__":
     size = 65565
